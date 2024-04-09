919b44cc5022c3f21efa854f41482f48083e696f
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-04-09 10:53:43 +0900

    Bank 인터페이스 추가(#3)

---
+++ firstWeekSeminar/src/main/java/org/sopt/firstweektask/bank/BankInterface.java
@@ -0,0 +1,15 @@
+package org.sopt.firstweektask.bank;
+
+import org.sopt.firstweektask.account.AccountInterface;
+
+//인터페이스 내의 메서드는 모두 public abstract이라 생략 가능하지만, 공부중이니 적어봄, (자바8부터 default 메소드와 static 메소드 사용가능)
+public interface BankInterface {
+    public abstract void createAccount();
+    public abstract void deposit();
+    public abstract void withdraw();
+    public abstract void transfer();
+    public abstract void checkBalance();
+    public abstract void closeScanner();
+    public abstract void addAccount(String accountNumber, AccountInterface account);
+    public abstract AccountInterface getAccount(String accountNumber);
+}
