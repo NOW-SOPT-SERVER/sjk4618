795f30b9f6e8949465d70f41f40f8593ca844cfd
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-04-16 04:30:47 +0900

    MemberService에서 getAllMemberList 메서드 구현(#5)

--- springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/service/MemberService.java
+++ springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/service/MemberService.java
@@ -6,8 +6,11 @@ import org.sopt.springFirstSeminar.domain.Member;
 import org.sopt.springFirstSeminar.repository.MemberRepository;
 import org.sopt.springFirstSeminar.service.dto.MemberCreateDTO;
 import org.sopt.springFirstSeminar.service.dto.MemberFindDTO;
+import org.sopt.springFirstSeminar.service.dto.MemberDataDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,17 +19,13 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional //데이터베이스의 변경사항을 적용하는(있을때) 어노테이션
-    public String createMember(
-           final MemberCreateDTO memberCreateDTO //final 이유 : 인자의 불변성보장
-    ) {
+    public String createMember(final MemberCreateDTO memberCreateDTO) { //final 이유 : 인자의 불변성보장
         Member member = Member.create(memberCreateDTO.name(),memberCreateDTO.part(), memberCreateDTO.age());
         memberRepository.save(member);
         return member.getId().toString();
     }
 
-    public MemberFindDTO findMemberById(
-            Long memberId
-    ) {
+    public MemberFindDTO findMemberById(Long memberId) {
         return MemberFindDTO.of(memberRepository.findById(memberId).orElseThrow(
                 () -> new EntityNotFoundException("ID에 해당하는 사용자가 존재하지 않습니다.")
         ));
@@ -39,4 +38,10 @@ public class MemberService {
         );
         memberRepository.delete(member);
     }
+
+    public List<MemberDataDTO> getAllMemberList() {
+        return memberRepository.findAll().stream().map(MemberDataDTO::of).toList();
+    }
+
+
 }
--- springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/service/dto/MemberDataDTO.java
+++ springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/service/dto/MemberDataDTO.java
@@ -3,14 +3,14 @@ package org.sopt.springFirstSeminar.service.dto;
 import org.sopt.springFirstSeminar.domain.Member;
 import org.sopt.springFirstSeminar.domain.Part;
 
-public record MemberListDataDTO(
+public record MemberDataDTO(
         Long id,
         String name,
         Part part,
         int age
 ) {
-    public static MemberListDataDTO of(Member member) {
-        return new MemberListDataDTO(member.getId(),member.getName(), member.getPart(), member.getAge());
+    public static MemberDataDTO of(Member member) {
+        return new MemberDataDTO(member.getId(),member.getName(), member.getPart(), member.getAge());
     }
 }
 
