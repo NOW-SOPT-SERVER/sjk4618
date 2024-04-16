c89ce545ea17f0a3f73dcefcaebd632a5a97ddda
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-04-17 05:38:21 +0900

    DTO 생성(#7)

--- carrotMarket/src/main/java/org/sopt/carrotMarket/repository/ItemRepository.java
+++ carrotMarket/src/main/java/org/sopt/carrotMarket/repository/ItemRepository.java
@@ -1,2 +1,7 @@
-package org.sopt.carrotMarket.repository;public interface ItemRepository {
+package org.sopt.carrotMarket.repository;
+
+import org.sopt.carrotMarket.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
