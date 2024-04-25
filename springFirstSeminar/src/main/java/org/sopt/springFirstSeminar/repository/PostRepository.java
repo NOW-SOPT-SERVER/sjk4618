2e396a51bfa4861017ee8a6818a4c9ceec7b08c0
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-04-25 20:58:06 +0900

    [Feat] PostRepository 생성(#9)

---
+++ springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package org.sopt.springFirstSeminar.repository;
+
+import org.sopt.springFirstSeminar.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
