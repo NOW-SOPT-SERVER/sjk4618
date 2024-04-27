dec8c2886157ada7301251b9fdbc21ddaac59a73
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-04-27 14:10:47 +0900

    에러 메세지, 성공메세지 추가(#9)

--- springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/common/dto/ErrorMessage.java
+++ springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/common/dto/ErrorMessage.java
@@ -11,6 +11,7 @@ public enum ErrorMessage {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 사용자가 존재하지 않습니다."),
     BLOG_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "BLOG ID에 해당하는 블로그가 존재하지 않습니다."),
     BLOG_NOT_MATCH_MEMBER(HttpStatus.NOT_FOUND.value(), "해당 멤버 ID에 해당하는 블로그가 존재하지 않습니다."),
+    CONTENT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "POST ID에 해당하는 글이 존재하지 않습니다")
     ;
 
     private final int status;
