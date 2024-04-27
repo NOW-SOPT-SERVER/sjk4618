e4af6be93b8e48531d28d19510e6813e211ff469
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-04-27 14:12:14 +0900

    [Feat] blogContentDTO 생성(#9)

--- springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/service/dto/BlogContentResponseDTO.java
+++ springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/service/dto/BlogContentResponseDTO.java
@@ -1,2 +1,22 @@
-package org.sopt.springFirstSeminar.service.dto;public record BlogContentResponseDTO() {
+package org.sopt.springFirstSeminar.service.dto;
+
+import lombok.Builder;
+import org.sopt.springFirstSeminar.domain.Blog;
+import org.sopt.springFirstSeminar.domain.Post;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record BlogContentResponseDTO(
+        String name,
+        String content,
+        String createdAt
+) {
+    public static BlogContentResponseDTO of(Post post, String createdAt) {
+        return BlogContentResponseDTO.builder()
+                .name(post.getName())
+                .content(post.getContent())
+                .createdAt(createdAt)
+                .build();
+    }
 }
