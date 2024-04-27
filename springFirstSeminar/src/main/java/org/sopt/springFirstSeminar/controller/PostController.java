6a2326d295f48b7e8ae65108834218553010b4c5
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-04-27 14:11:53 +0900

    [Feat] PostController 생성(#9)

--- springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/controller/PostController.java
+++ springFirstSeminar/src/main/java/org/sopt/springFirstSeminar/controller/PostController.java
@@ -3,9 +3,10 @@ package org.sopt.springFirstSeminar.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.sopt.springFirstSeminar.common.dto.SuccessMessage;
-import org.sopt.springFirstSeminar.common.dto.SuccesttStatusResponse;
+import org.sopt.springFirstSeminar.common.dto.SuccessStatusResponse;
 import org.sopt.springFirstSeminar.service.PostService;
 import org.sopt.springFirstSeminar.service.dto.BlogContentRequestDTO;
+import org.sopt.springFirstSeminar.service.dto.BlogContentResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,15 +19,26 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/post/{blogId}")
-    public ResponseEntity<SuccesttStatusResponse> postBlogContent(
+    public ResponseEntity<SuccessStatusResponse<?>> postBlogContent(
             @RequestHeader(name = "memberId") Long memberId,
             @PathVariable Long blogId,
             @Valid @RequestBody BlogContentRequestDTO blogContentRequestDTO
             ) {
+
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .header("Location", postService.postContent(memberId, blogId, blogContentRequestDTO))
-                .body(SuccesttStatusResponse.of(SuccessMessage.BLOG_CONTENT_CREATE_SUCCESS));
+                .body(SuccessStatusResponse.of(SuccessMessage.BLOG_CONTENT_CREATE_SUCCESS));
+    }
+
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<SuccessStatusResponse<?>> getPostContent(
+            @PathVariable Long postId
+    ) {
+        final BlogContentResponseDTO response = postService.getBlogContent(postId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SuccessStatusResponse.of(SuccessMessage.GET_BLOG_CONTENT_SUCCESS, response));
     }
 
 
